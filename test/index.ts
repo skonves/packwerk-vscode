f18e6b8711fc8917119351696f022fd2d3684aba
Steve Konves <stephen.konves@gmail.com>
2021-12-03 10:28:03 -0700

    chore: install updated vscode dependencies

--- test/index.ts
+++ test/index.ts
@@ -1,10 +1,10 @@
-import * as testRunner from 'vscode/lib/testrunner';
+// import * as testRunner from 'vscode-test/out';
 
-// see https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
-testRunner.configure({
-  reporter: 'spec',
-  ui: 'bdd',
-  useColors: true,
-});
+// // see https://github.com/mochajs/mocha/wiki/Using-mocha-programmatically#set-options for more info
+// testRunner.configure({
+//   reporter: 'spec',
+//   ui: 'bdd',
+//   useColors: true,
+// });
 
-module.exports = testRunner;
+// module.exports = testRunner;
